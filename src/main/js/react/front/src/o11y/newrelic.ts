dca42c24f6721f2f2649c8749b2d0aa8f3c7b55c
Takuya Azuma <tazuma@newrelic.com>
2024-02-22 16:42:40 +0900

    first commit

--- src/main/js/react/front/src/o11y/newrelic.ts
+++ src/main/js/react/front/src/o11y/newrelic.ts
@@ -7,8 +7,9 @@ const options = {
 }
 
 const prodOptions = {
-    init: {distributed_tracing:{enabled:true},privacy:{cookies_enabled:true}},
-    info: {beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",licenseKey:"NRJS-d5fc04e71a6af75ae7d",applicationID:"1588910227",sa:1},
-    loader_config: {accountID:"3873151",trustKey:"3830171",agentID:"1588910227",licenseKey:"NRJS-d5fc04e71a6af75ae7d",applicationID:"1588910227"},
+    init: {session_replay:{enabled:true,block_selector:'',mask_text_selector:'*',sampling_rate:100.0,error_sampling_rate:100.0,mask_all_inputs:true,collect_fonts:true,inline_images:false,inline_stylesheet:true,mask_input_options:{}},distributed_tracing:{enabled:true},privacy:{cookies_enabled:true},ajax:{deny_list:["bam.nr-data.net"]}},
+    info: {beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",licenseKey:"NRJS-c74178fae190d128df7",applicationID:"1588920133",sa:1},
+    loader_config: {accountID:"4365310",trustKey:"3830171",agentID:"1588920133",licenseKey:"NRJS-c74178fae190d128df7",applicationID:"1588920133"},
 }
+
 export default new BrowserAgent(process.env.NODE_ENV === 'production' ? prodOptions : options)
