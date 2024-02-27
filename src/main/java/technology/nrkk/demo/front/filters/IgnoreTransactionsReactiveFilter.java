ba1a502d30037c0385d0b9dad4ec9c33fcf36441
Takuya Azuma <tazuma@newrelic.com>
2024-02-27 11:32:25 +0900

    first commit

---
+++ src/main/java/technology/nrkk/demo/front/filters/IgnoreTransactionsReactiveFilter.java
@@ -0,0 +1,34 @@
+package technology.nrkk.demo.front.filters;
+
+import com.newrelic.api.agent.NewRelic;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.regex.Pattern;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class IgnoreTransactionsReactiveFilter implements WebFilter {
+
+    private static final String ACTUATOR_ENDPOINT_PATTERN = "^(/actuator|/favicon).*";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+
+        boolean ignoreTx = exchange.getRequest().getPath().value()
+            .matches(ACTUATOR_ENDPOINT_PATTERN);
+
+
+        if (ignoreTx){
+            NewRelic.ignoreTransaction();
+        }
+
+
+        return chain.filter(exchange);
+    }
+}
