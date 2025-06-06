c324395c6a2bb24576ba342366114e5dedd1af59
Azumaxoid <azumax.android@gmail.com>
2025-06-06 20:31:34 +0900

    change pre to post

--- src/main/java/technology/nrkk/demo/front/filters/TransactionNamingReactiveFilter.java
+++ src/main/java/technology/nrkk/demo/front/filters/TransactionNamingReactiveFilter.java
@@ -7,8 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE + 1)
@@ -16,7 +18,7 @@ public class TransactionNamingReactiveFilter implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(TransactionNamingReactiveFilter.class);
     private static final String ACTUATOR_ENDPOINT_PATTERN = "^(/actuator|/favicon|/static|/login|/health).*";
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
 
         String path = request.getRequestURI();
         String method = request.getMethod();
@@ -26,14 +28,11 @@ public class TransactionNamingReactiveFilter implements HandlerInterceptor {
                     .matches(ACTUATOR_ENDPOINT_PATTERN);
             if (ignoreTx) {
                 NewRelic.ignoreTransaction();
-                return true;
             } else {
                 String transactionName = String.format("%s (%s)", path, method);
                 NewRelic.setTransactionName(null, transactionName);
             }
         }
-
-        return true;
     }
 
 }
