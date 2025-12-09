381e7a79c55034d97e5db24c28199cee79980c38
Azumaxoid <azumax.android@gmail.com>
2025-12-09 15:52:33 +0900

    fix filter from interceptor to filter

--- src/main/java/technology/nrkk/demo/front/filters/TransactionNamingFilter.java
+++ src/main/java/technology/nrkk/demo/front/filters/TransactionNamingFilter.java
@@ -1,27 +1,29 @@
 package technology.nrkk.demo.front.filters;
 
 import com.newrelic.api.agent.NewRelic;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 @Component
-@Order(Ordered.HIGHEST_PRECEDENCE + 1)
-public class TransactionNamingFilter implements HandlerInterceptor {
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class TransactionNamingFilter implements Filter, Ordered {
     private static final Logger logger = LoggerFactory.getLogger(TransactionNamingFilter.class);
     private static final String ACTUATOR_ENDPOINT_PATTERN = "^(/actuator|/favicon|/static|/login|/health).*";
 
     private static final String START_TIME = "startTime";
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
         String path = request.getRequestURI();
         String method = request.getMethod();
 
@@ -39,25 +41,27 @@ public class TransactionNamingFilter implements HandlerInterceptor {
 
         long startTime = System.currentTimeMillis();
         request.setAttribute(START_TIME, startTime);
-        return true;
-    }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        String method = request.getMethod();
-        String path = request.getRequestURI();
+        chain.doFilter(request, response);
+
+        // After completion
         String replacesPath = "/error".equals(path) ? (String) request.getAttribute("jakarta.servlet.error.request_uri") : path;
         String contentType = request.getContentType();
         int status = response.getStatus();
         int size = response.getBufferSize();
-        long startTime = (long) request.getAttribute(START_TIME);
+        long endStartTime = (long) request.getAttribute(START_TIME);
         long endTime = System.currentTimeMillis();
-        long duration = endTime - startTime;
+        long duration = endTime - endStartTime;
         if (status > 500) {
             logger.error("%s %s %s %d %d %d ms".formatted(method, replacesPath, contentType == null ? "-" : contentType, status, size, duration));
         } else {
             logger.info("%s %s %s %d %d %d ms".formatted(method, replacesPath, contentType == null ? "-" : contentType, status, size, duration));
         }
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 
 }
